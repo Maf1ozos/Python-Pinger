a95e7e09f84afe0e6f7da3463d704579b2425145
Maf1ozos <98760909+Maf1ozos@users.noreply.github.com>
2022-01-31 18:26:55 +0200

    Add files via upload
    A Simple Pinger Made With Python

---
+++ Ip Pinger.py
@@ -0,0 +1,23 @@
+#note this is not a dos or ddos program!
+import os
+
+print("""
+          ╔═══╗
+          ║╔══╝
+          ║╚══╦═╦══╦═══╦══╦═╗
+          ║╔══╣╔╣╔╗╠══║║║═╣╔╗╗
+          ║║──║║║╚╝║║══╣║═╣║║║
+          ╚╝──╚╝╚══╩═══╩══╩╝╚╝
+
+          An Average Ip Pinger 
+
+>Made By !Maf1ozos#6666
+""")
+      
+enter_ip = input('Put Ip: ')
+
+
+os.system('ping -t {}'.format(enter_ip))
+
+
+
